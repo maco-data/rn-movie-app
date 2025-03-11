296ddd19958f6e03d6e84f3e32d6818805f6f3fa
maco-data <mcospino@gmail.com>
2025-03-11 01:49:03 -0500

    Creat app screens routing and implemented navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,18 @@
+import React from 'react'
 import { Stack } from "expo-router";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="movie/[id]"
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  )
 }
